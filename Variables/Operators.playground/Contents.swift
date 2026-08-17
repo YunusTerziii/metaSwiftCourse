9c740bc45e5b52a687bce5167c3097ccc6f7af68
yunusterziii <ynsterzi@gmail.com>
2026-08-18 01:38:17 +0300

    Swift playgrounds notes

---
+++ Variables/Operators.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let levelScore = 10
+var gameScore = 0
+
+gameScore += levelScore
+print("Game Score: \(gameScore)")
+
+var levelBonusScore = 10.0
+levelBonusScore = 20
+
+print("The level's bonus score is :\(levelBonusScore)")
+
+gameScore += Int(levelBonusScore)
+print("Game Score: \(gameScore)")
+
+let levelLowestScore = 50
+let levelHighestScore = 99
+let levels = 10
+
+let levelScoreDifference = levelHighestScore - levelLowestScore
+let levelAverageScore = levelScoreDifference / levels
+print("The level's average score is :\(levelAverageScore)")
+let averageLevelScore = Double(levelScoreDifference) / Double(levels)
+print("The level's real average score is :\(averageLevelScore)")
