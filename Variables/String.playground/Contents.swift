a7033948b73fd29cec419f76040ea1632f1691a2
yunusterziii <ynsterzi@gmail.com>
2026-08-18 02:03:34 +0300

    String notes

--- Variables/String.playground/Contents.swift
+++ Variables/String.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let day = "Monday"
+print("Today is \(day)")
+let hour = "6"
+let minutes = "15"
+let period = "PM"
+
+var time = hour + ":" + minutes + " " + period
+print("The time is \(time)")
+
+print("Today is \(day) and the time is \(time)")
+let timezone = "PST"
+time += " \(timezone)"
+print("The time is \(time)")
+
+let shortDay = day.prefix(3)
+print("The short day is \(shortDay).")
+
+print("Today is \(shortDay). and the time is \(time)")
+
